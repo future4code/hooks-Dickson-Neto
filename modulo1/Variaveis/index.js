e593507ebd752074da81a0b4717bcaf4da6d2142
Dickson Freitas <dickfreitas12@gmaill.com>
2022-02-08 16:04:35 -0300

    JavaScript

---
+++ modulo1/Variaveis/index.js
@@ -0,0 +1,24 @@
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+// console.log (b)= 10
+// console.log (a b) = 10 , 5
+
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log ( a , b , c)
+// console.log (a , b, c)= 10,10,10
+
+let TrabalhoHr = prompt("Quantas horas você trabalha por dia?")
+let ValorDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
