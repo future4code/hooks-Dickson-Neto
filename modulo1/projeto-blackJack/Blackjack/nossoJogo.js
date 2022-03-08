5616ed0082f847e6cfc0758fae85576cd812a032
Dickson Freitas <dickfreitas12@gmaill.com>
2022-03-08 18:29:32 -0300

    FALTA O DESAFIO

---
+++ modulo1/projeto-blackJack/Blackjack/nossoJogo.js
@@ -0,0 +1,38 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+// if(confirm("Quer iniciar a nova rodada?")){
+//    console.log ("Bem vindo ao jogo de Black Jack")
+//    const cartaUsuario = comprarCarta()
+// const carta2Usuario = comprarCarta()
+// const pontuação = (cartaUsuario.valor + carta2Usuario.valor)
+
+// const cartaComputador = comprarCarta()
+// const carta2Computador = comprarCarta()
+// const pontuaçãoComputador = (cartaComputador.valor + carta2Computador.valor)
+
+// console.log (`Usuario -  CARTAS :  ${cartaUsuario.texto} ${carta2Usuario.texto} - pontuação ${pontuação}`)
+// console.log (`Computador CARTAS : ${cartaComputador.texto}  ${carta2Computador.texto} - pontuação ${pontuaçãoComputador}`)
+
+// if(pontuação > pontuaçãoComputador) {
+//    console.log (" O usuario GANHOU")
+// }else if (pontuaçãoComputador > pontuação){
+//    console.log ( " O computador GANHOU")
+// }else {
+//    console.log (" EMPATE ")
+// }
+// }else{
+//  console.log ("O JOGO ACABOU")
+   
+// }
+
