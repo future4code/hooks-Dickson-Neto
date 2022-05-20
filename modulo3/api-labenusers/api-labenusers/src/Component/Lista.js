e82f0d840313e4434ed937981bdd4ce88a923d83
Dickson Freitas <dickfreitas12@gmaill.com>
2022-05-19 22:00:09 -0300

    Todas as funcoes FUNCIONAAAAAAM

---
+++ modulo3/api-labenusers/api-labenusers/src/Component/Lista.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+class Lista extends React.Component{
+    render(){
+        return(
+            <div>
+                <button>Trocar Pagina</button>
+            </div>
+        )
+    }
+}
+
+export default Lista
