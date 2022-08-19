236148f1728c8b21ea8777b9e05f6be772e7ed82
Dickson Freitas <dickfreitas12@gmaill.com>
2022-08-18 22:09:00 -0300

    Exercicios ok

---
+++ modulo5/aprofundamento-express/src/data.ts
@@ -0,0 +1,40 @@
+type Afazeres = {
+    userName : string,
+    id:string,
+    name : string,
+    done : boolean
+}
+
+export let arrFilmes: Afazeres[] = [
+    {
+        userName : "Dickson",
+        id:'8b4215f1-950b-4c06-bfd6-80c2f8d1bc49',
+        name : "Capitão América: O Primeiro Vingador",
+        done : true
+    },
+    {
+        userName :  "Dickson",
+        id:"28e07ef5-4c6f-4478-9998-1fae4c6d4543",
+        name : "Agent Carter",
+        done : false
+    },
+    {
+        userName :  "Dickson",
+        id:'8b4215f1-950b-4c06-bfd6-80c2f8d1cc08',
+        name : "Capitã Marvel",
+        done : true
+    },
+    {
+        userName :  "Dickson",
+        id:'8b4215f1-950b-4c06-bfd6-80c2f8d1bc75',
+        name : "Vingadores: Era de Ultron",
+        done : false
+    },
+    {
+        userName :  "Dickson",
+        id:'8b4215f1-950b-4c06-bfd6-54c2f6d2bc65',
+        name : "Homem-Aranha: De Volta ao Lar ",
+        done : true
+    }
+    
+]
