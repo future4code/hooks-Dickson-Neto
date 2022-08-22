062c1d463f58a3b902775f1298928b367312f974
Dickson Freitas <dickfreitas12@gmaill.com>
2022-08-22 16:40:38 -0300

    Algumas funçoes

---
+++ modulo5/projeto-Labebank/src/data.ts
@@ -0,0 +1,51 @@
+import { SelectionRange } from "typescript"
+
+//VERIFICAÇÃO DE DATAS
+const data = new Date();
+const day = String(data.getDate());
+const month = String(data.getMonth() + 1).padStart(2, "0");
+export const year = data.getFullYear();
+export const today = day + "/" + month + "/" + year;
+
+//TIPAGEM DOS USUARIOS DO BANCO
+export type Users = {
+    name: string,
+    CPF : string,
+    birthDate : string,
+    balance : number,
+    bankStatemente : Transaction[]
+}
+
+//AQUI IREI FAZER AS TRANSAÇOES PEDIDAS
+export type Transaction = {
+        date : string,
+        amount : number,
+        description : string,
+    }
+
+//DADOS DOS USUARIOS PARA O BANCO
+export let usersBank : Users[] = [
+   {
+    name : "Dickson",
+    CPF : "11111111123",
+    birthDate : "11/02/1999",
+    balance : 0,
+    bankStatemente : []
+   },
+
+   {
+    name : "Karl Josefsen",
+    CPF : "97136590078",
+    birthDate : "26/12/1994",
+    balance : 0,
+    bankStatemente : []
+   },
+
+   {
+    name : "Mary Gocher",
+    CPF : "28004499007",
+    birthDate : "19/01/1984",
+    balance : 0,
+    bankStatemente : []
+   }
+]
---
+++ modulo5/projeto-Labebank/src/index.ts
@@ -0,0 +1,124 @@
+import express , {Express , Request , Response}  from "express";
+import cors from 'cors';
+import { usersBank, Users, Transaction , today , year } from "./data";
+
+const app : Express = express()
+
+app.use(express.json())
+app.use(cors())
+
+//PEGAR TODOS OS USUARIOS DO BANCO
+app.get("/users" , (req :Request, res:Response ) =>{
+    res.send(usersBank)
+})
+
+//CRIAR NOVO USUARIO
+app.post("/createUsers" ,  (req :Request, res:Response ) =>{
+    const {name , CPF , birthDate } = req.body
+    const yearBirth = birthDate.split("/")
+
+    let idade : Number = year  - yearBirth[2] 
+
+    if(idade < 18){
+        throw new Error ("Você deve ter no minimo 18 anos para criar uma conta")
+    }else {
+        const newUsers : Users  = {
+            name,
+            CPF,
+            birthDate,
+            balance : 0,
+            bankStatemente : [],
+        }
+
+        usersBank.push(newUsers)
+    }
+res.send("Usuario Criado com sucesso")
+
+})
+
+//PEGAR SALDO DA CONTA 
+app.get("/users/balance" , (req :Request, res:Response) =>{
+   const cpf = req.headers.cpf
+    if(!cpf){
+        throw new Error ()   
+    }
+    try{
+        const index = usersBank.findIndex(client => client.CPF === cpf)
+        res.send(`${usersBank[index].name}, seu saldo é:R$ ${usersBank[index].balance} `)
+    }catch{
+        throw new Error("CPF não condiz com nenhuma conta cadastrada!!")
+    }
+}) 
+
+//ADICIONANDO SALDO A UMA CONTA 
+app.post("/myAccount" , (req : Request , res : Response) =>{
+    const {name , cpf , cash } = req.body
+    if(!name || !cpf || !cash){
+        throw new Error ("Faltam informações para o deposito")
+    }
+    const index = usersBank.findIndex(client => client.name === name && client.CPF === cpf)
+    
+    try{
+        if(index != -1){
+            usersBank[index].balance = usersBank[index].balance + cash
+            const newDeposit = {
+                date : today,
+                amount : cash,
+                description : "Saldo adicionado com sucesso"
+            }
+            usersBank[index].bankStatemente.push(newDeposit)
+            res.send(`${usersBank[index].name} depositou ${cash}. Seu saldo atual é ${usersBank[index].balance}`)
+        }
+    }catch{
+        throw new Error ("Algo esta errado, ligue para o seu gerente!")
+
+    }
+})
+
+//PAGANDO UMA CONTA COM O VALOR DA CONTA
+app.put("/myAccount/pay" , (req : Request , res : Response) =>{
+    const {cpf} = req.headers
+    const {payment_date , description , cash} = req.body
+
+    const index = usersBank.findIndex(client => client.CPF === cpf)
+    const valorTotal = usersBank[index].balance - cash
+     try{
+        if(index ) {
+            if(payment_date){
+                const newBill : Transaction = {
+                    date : payment_date ,
+                    description,
+                    amount : valorTotal
+                }
+                //VERFICAÇÃO PARA NÃO DEIXAR PAGAR A CONTA SEM TER O VALOR DEPOSITADO
+            if(valorTotal < 0){
+                res.send("Valor insuficiente")
+
+            }else{
+                usersBank[index].bankStatemente.push(newBill)
+                res.send(`Pagamento realizado com sucesso. ${usersBank[index].name} ,  seu saldo atual é ${valorTotal}`)
+            }
+                    
+            }else{
+                //CASO NÃO PASSE A DATA NO BODY, ELE RECONHECERA A DATA ATUAL PARA O PAGAMENTO DA CONTA
+                const newBill : Transaction = {
+                    date : today,
+                    description,
+                    amount : cash
+                }
+                usersBank[index].balance =  usersBank[index].balance - cash
+                usersBank[index].bankStatemente.push(newBill)
+            }
+            //ESSAS VERIFICAÇOES DE ERRO NAO ESTÃO  FUNCIONANDO!!
+        }else{
+            throw new Error ("Verifique suas informações")
+        }
+    }catch (error){
+        throw new Error ("Verifique suas informações")
+    }
+   
+})
+
+app.listen("3003" , () =>{
+    console.log('O servidor esta online na porta 3003')
+})
