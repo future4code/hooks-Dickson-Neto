f7779bc49c656487d115e8c7f4abf91abcd709c3
Dickson Freitas <dickfreitas12@gmaill.com>
2022-09-11 00:28:58 -0300

    Desafio 6-7-8

--- modulo5/to-do-list/src/database/TodoList.database.ts
+++ modulo5/to-do-list/src/database/TodoList.database.ts
@@ -2,32 +2,61 @@ import { Database } from "../migrations/connection";
 import { newList } from "../models/TodoList";
 import { TodoListRepository } from "../models/TodoListRepository";
 import { UserTask } from "../models/TodoListTask";
-import { ListTask } from "../services/types";
+import { ListTask, TodoList } from "../services/types";
 
+const list_table = "TodoListUser";
+const task_table = "TodoListTask";
 
+export default class TodoListDatabase
+  extends Database
+  implements TodoListRepository
+{
+  public async createUserList(list: newList): Promise<void> {
+    return await Database.connection(list_table)
+    .insert(list);
+  }
 
-const list_table = "TodoListUser"
-const task_table = "TodoListTask"
+  public async getUserById(id: string) {
+    return await Database.connection(list_table)
+    .select("*")
+    .where({ id: id });
+  }
 
-export default class TodoListDatabase extends Database implements TodoListRepository{
+  public async editUserById(list: newList, id: string): Promise<number> {
+    return await Database.connection(list_table)
+    .update(list)
+    .where({ id: id });
+  }
 
-    public async createUserList(list : newList ) : Promise<void>{
-       return await Database.connection(list_table).insert(list)
-    }
+  public async takeUser(): Promise<TodoList[]> {
+    const result = await Database.connection(list_table)
+    .select("*");
+    return result.flat(1);
+  }
 
-    public async getUserById(id : string) {
-        return await Database.connection(list_table).select("*").where({id : id})
-    }
+ 
 
-    public async editUserById(list : newList , id : string) : Promise<number> {
-    return await Database.connection(list_table).update(list).where({id : id})
-    }
+  public async searchUser(query : string){
+    const result = await Database.connection(list_table).select("*").where({email : query})
+    return result
+  }
 
-    public async createTask(list : UserTask) : Promise<void>{
-        return await Database.connection(task_table).insert(list)
-    }
+  //TAREFAS
+  public async createTask(list: UserTask): Promise<void> {
+    return await Database.connection(task_table)
+    .insert(list);
+  }
 
-    public async deleteTaskById(id : string) : Promise<number>{
-        return await Database.connection(task_table).delete().where({id : id })
-    }
+  public async deleteTaskById(id: string): Promise<number> {
+    return await Database.connection(task_table)
+    .delete()
+    .where({ id: id });
+  }
+  
+  public async takeTaskUserId(idUser: string) {
+    const result = await Database.connection(task_table)
+      .select("*")
+      .where({ criador_user_id: idUser });
+    return result;
+  }
 }
--- modulo5/to-do-list/src/models/TodoListRepository.ts
+++ modulo5/to-do-list/src/models/TodoListRepository.ts
@@ -1,12 +1,16 @@
-import { TodoList } from "../services/types";
+import { ListTask, TodoList } from "../services/types";
 import { newList } from "./TodoList";
 import { UserTask } from "./TodoListTask";
 
-
 export interface TodoListRepository {
-    createUserList(list : newList) : Promise<void>
-    getUserById(id : string) : Promise<TodoList[]>
-    editUserById(list : newList , id : string) : Promise<number>
-    createTask(list : UserTask) : Promise<void>
-    deleteTaskById(id : string) : Promise<number>
+  createUserList(list: newList): Promise<void>;
+  getUserById(id: string): Promise<TodoList[]>;
+  editUserById(list: newList, id: string): Promise<number>;
+  takeUser(): Promise<TodoList[]>;
+  searchUser(query : string) : Promise<TodoList[]>
+
+  //TAREFAS
+  createTask(list: UserTask): Promise<void>;
+  deleteTaskById(id: string): Promise<number>;
+  takeTaskUserId(idUser: string): Promise<ListTask[]>;
 }
